c6f5e966d3c0c5287c131d4f47ad78b37ed9a7f3
UChourasiya13 <ujjwalchourasiya40287@gmail.com>
2024-05-27 13:27:39 +0530

    Tisha 👻

--- area.cpp
+++ area.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
-#include<stdio.h>
+#include <iostream>
+#include <stdio.h>
 using namespace std;
 
-int main(){
-    float area,l,b;
-    cout<<"Enter length and breadth of triangle:";
-    cin>>l>>b;
-    area=0.5*l*b;
-    cout<<"Area of triangle is:" <<area;
+int main()
+{
+    float area, len, bre;
+    cout << "Enter length and breadth of triangle:";
+    cin >> len >> bre;
+    area = 0.5 * len * bre;
+    cout << "Area of triangle is:" << area;
     return 0;
-
 }
